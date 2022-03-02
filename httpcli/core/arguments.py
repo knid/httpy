c55b0a1c641b4100c87e0f736122770525d9b8f9
SinanKanidagli <sinan_kanidagli@hotmail.com>
2022-03-02 17:59:35 +0300

    update argument parser

--- httpcli/core/arguments.py
+++ httpcli/core/arguments.py
@@ -1,54 +1,87 @@
+import argparse
 import sys
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 current_path = Path(__file__).parent.parent.parent.resolve()
 sys.path.append(str(current_path))
 
 
-class Arguments:
+class ArgumentParser(argparse.ArgumentParser):
     HEADER_MARK = ":"
     BODY_ARG_MARK = "="
-    URL_ARG_MARK = "=="
-    METHODS = {
-        "GET": "GET",
-        "POST": "POST",
-        "PUT": "PUT",
-        "PATCH": "PATCH",
-    }
+    QUERY_MARK = "=="
 
     def __init__(self, args: List[str]) -> None:
-        self.args = args
+        super().__init__()
         self._headers: Dict[str, Any] = dict()
-        self._arguments: Dict[str, Any] = dict()
+        self._query_arguments: Dict[str, Any] = dict()
         self._body: Dict[str, Any] = dict()
 
+        self.description = """Simple HTTP Cli tool for API. JSON requests,
+        downloads, functionality,variables and colorized responses"""
+        self.prog = "httpcli"
+
+        self.add_argument("-v", "--version", action="version", version="%(prog)s 0.0.1")
+        self.add_argument(
+            "method", default="GET", nargs="?", choices=["GET", "POST", "PUT", "DELETE"]
+        )
+        self.add_argument(
+            "-H", "--header", action="store_true", help="show response header only"
+        )
+        self.add_argument(
+            "-B", "--body", action="store_true", help="show response body only"
+        )
+        self.add_argument(
+            "-S", "--status", action="store_true", help="show response status only"
+        )
+        self.add_argument(
+            "-V",
+            "--verbose",
+            action="store_true",
+            help="show request and response in verbose",
+        )
+        self.add_argument(
+            "-r",
+            "--allow-redirect",
+            action="store_true",
+            help="allow requests to be redirected",
+        )
+
+        self.args = self.parse_args()
+
     @property
-    def headers(self) -> Dict[str, Any]:
+    def headers(self) -> Union[Dict[str, Any], None]:
         for arg in self.args:
-            if Arguments.HEADER_MARK in arg:
-                val = arg.split(Arguments.HEADER_MARK)
+            if ArgumentParser.HEADER_MARK in arg:
+                val = arg.split(ArgumentParser.HEADER_MARK)
                 self._headers[val[0]] = val[1]
-        return self._headers
+        if self._headers:
+            return self._headers
+        return None
 
     @property
     def body(self) -> Dict[str, Any]:
         for arg in self.args:
-            if Arguments.BODY_ARG_MARK in arg and Arguments.URL_ARG_MARK not in arg:
-                val = arg.split(Arguments.BODY_ARG_MARK)
+            if (
+                ArgumentParser.BODY_ARG_MARK in arg
+                and ArgumentParser.QUERY_MARK not in arg
+            ):
+                val = arg.split(ArgumentParser.BODY_ARG_MARK)
                 self._body[val[0]] = val[1]
-        return self._body
+        if self._body:
+            return self._body
+        return None
 
     @property
-    def arguments(self) -> Dict[str, Any]:
+    def query_arguments(self) -> Dict[str, Any]:
         for arg in self.args:
-            if Arguments.URL_ARG_MARK in arg:
-                val = arg.split(Arguments.URL_ARG_MARK)
-                self._arguments[val[0]] = val[1]
-        return self._arguments
+            if ArgumentParser.QUERY_MARK in arg:
+                val = arg.split(ArgumentParser.QUERY_MARK)
+                self._query_arguments[val[0]] = val[1]
+        if self._query_arguments:
+            return self._query_arguments
+        return None
 
-    @property
-    def method(self) -> str:
-        for arg in self.args:
-            if arg.upper() in Arguments.METHODS:
-                return arg.upper()
+
+print(ArgumentParser(sys.argv[1:]).args)
--- httpcli/httpcli.py
+++ httpcli/httpcli.py
@@ -1,1 +1,10 @@
+# import requests
+# from typing import List
+
 # from core.arguments import Arguments
+
+
+# class HTTPCli():
+#     def __init__(self, args: List[str]) -> None:
+#         self.args = Arguments(args)
+#         self.method = self.args.method
