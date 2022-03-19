2b8cb700e7237900fa763e9e2ea234724da74d32
SinanKanidagli <sinan_kanidagli@hotmail.com>
2022-03-19 20:20:31 +0300

    edit output funcs

--- httpy/output/http.py
+++ httpy/output/http.py
@@ -5,8 +5,10 @@ from httpy.output.lexers import print_html, print_json
 
 
 def print_status(res: requests.Response):
+    res.request.method
     print(
-        f"{Colors.RED}{Colors.BOLD}HTTP/1.1{Colors.ENDC}{Colors.CYAN} {res.status_code} {res.reason}"
+        f"{Colors.RED}{Colors.BOLD}{res.request.method} HTTP/1.1\
+            {Colors.ENDC}{Colors.CYAN} {res.status_code} {res.reason}"
     )
 
 
@@ -20,7 +22,9 @@ def print_body(res: requests.Response):
     json_content_type = "application/json"
     html_content_type = "text/html"
 
-    content_type = res.headers["content-type"]
+    content_type = html_content_type
+    if res.headers.__contains__("content-type"):
+        content_type = res.headers["content-type"]
 
     if content_type.startswith(json_content_type):
         print_json(res)
