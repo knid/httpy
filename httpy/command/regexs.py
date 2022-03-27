938a09bf1f3e09d2fa119223800cf8dfedb884c6
SinanKanidagli <sinan_kanidagli@hotmail.com>
2022-03-27 22:38:50 +0300

    fix read file

--- httpy/command/regexs.py
+++ httpy/command/regexs.py
@@ -3,5 +3,5 @@ class CommandRegexs:
     INCREMENT = r"\+{2}"
     DEINCREMENT = r"-{2}"
     RAND = r"rand[(][0-9]*[,][0-9]*[)]"
-    READ = r"read[(][a-zA-Z0-9/\\\.]*[)]"
+    READ = r"read[(][a-zA-Z0-9\/_\\\.]*[)]"
     TEXT = r".*"
